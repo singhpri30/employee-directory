901bb4a1b5c01f43372b8728437289779cd578de
Pri_030 <priyankasingh30@gmail.com>
2020-08-04 16:33:55 -0400

    updated bootstrap classes

--- src/component/SearchUser.js
+++ src/component/SearchUser.js
@@ -3,19 +3,19 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function SearchUser(props) {
   return (
-    <form>
-      <div className="form-group">
-        <label htmlFor="search">Search:</label>
+    <form className="text-center">
+      <div className="form-group m-auto">
+        <label className="mr-2" htmlFor="search"><strong>Search:</strong></label>
         <input
           onChange={props.handleInputChange}
           value={props.search}
           name="search"
           type="text"
-          className="form-control"
+          className="d-inline"
           placeholder="Search for a Gif"
           id="search"
         />
-        <button onClick={props.handleFormSubmit} className="btn btn-primary mt-3">
+        <button onClick={props.handleFormSubmit} className="btn btn-primary ml-2">
           Search
         </button>
       </div>
