19c7409c7916c538d3faeffe4569109c61d2504e
Pri_030 <priyankasingh30@gmail.com>
2020-08-04 00:37:16 -0400

    craeted nav component and added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import NavigationBar from './component/NavigationBar';
 
 function App() {
   return (
     <div className="App">
       
-      Hello
+      <NavigationBar></NavigationBar>
     </div>
   );
 }
