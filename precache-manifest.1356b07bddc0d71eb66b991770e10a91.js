d28bca29897e6428b8f78245920eddd51aa7de99
Pri_030 <priyankasingh30@gmail.com>
2020-08-06 22:36:40 -0400

    Updates

---
+++ precache-manifest.1356b07bddc0d71eb66b991770e10a91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d4e472768abda14f4537f9bb628b5e",
+    "url": "/singhpri30/employee-directory/index.html"
+  },
+  {
+    "revision": "beffad919225fa1e0990",
+    "url": "/singhpri30/employee-directory/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "29d7e4032f6bd7781b48",
+    "url": "/singhpri30/employee-directory/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "beffad919225fa1e0990",
+    "url": "/singhpri30/employee-directory/static/js/2.f2f04517.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/singhpri30/employee-directory/static/js/2.f2f04517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d7e4032f6bd7781b48",
+    "url": "/singhpri30/employee-directory/static/js/main.bf0240f0.chunk.js"
+  },
+  {
+    "revision": "31bd4027336ec9a142a2",
+    "url": "/singhpri30/employee-directory/static/js/runtime-main.379f96cc.js"
+  }
+]);
