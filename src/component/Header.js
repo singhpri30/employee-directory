c17cb8d806d258e1efbc6b4f0e6458afa15fef43
Pri_030 <priyankasingh30@gmail.com>
2020-08-06 23:39:30 -0400

    added fontawesome class

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAddressBook } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
   return (
     
      <header className="w-100 jumbotron mt-2 h-25 bg-info">
-       <h2 className="text-center  mt-2">Employee Directory</h2>
+       <h2 className="text-center  mt-2"><FontAwesomeIcon icon={faAddressBook} /> Employee Directory</h2>
      </header>
     
   )
