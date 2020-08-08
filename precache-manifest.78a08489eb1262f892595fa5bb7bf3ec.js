b9c764c544488a6fa69d98c65f4efd9f09515326
Pri_030 <priyankasingh30@gmail.com>
2020-08-08 12:00:29 -0400

    Updates

---
+++ precache-manifest.78a08489eb1262f892595fa5bb7bf3ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f808dcb98c909035a7d1736541b1821",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "f858b6f95066af045c59",
+    "url": "/employee-directory/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "56fa415c9ac9427b705f",
+    "url": "/employee-directory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f858b6f95066af045c59",
+    "url": "/employee-directory/static/js/2.42ec6e77.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/employee-directory/static/js/2.42ec6e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56fa415c9ac9427b705f",
+    "url": "/employee-directory/static/js/main.64a93b3d.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/singhpri30/employee-directory/precache-manifest.03ba93be4719c9bc279f790a4422c2be.js"
+  "/employee-directory/precache-manifest.78a08489eb1262f892595fa5bb7bf3ec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/singhpri30/employee-directory/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/employee-directory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
